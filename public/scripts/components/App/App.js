d60d7ca90eff5c8f733654dbae27445a43f43469
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2018-03-30 14:44:59 +0300

    Style Tag Component

--- public/scripts/components/App/App.js
+++ public/scripts/components/App/App.js
@@ -4,13 +4,19 @@ import React from 'react';
 // import { Button } from '../Button/Button';
 // import { IconButton } from '../IconButton/IconButton';
 // import { TextInput } from '../Input/TextInput';
+import { Tag } from '../Tag/Tag';
 import { Avatar } from '../Avatar/Avatar';
+import { Icon } from '../Icon/Icon';
 
 export const App = () => (
   <div>
-    <Avatar
-      size="medium"
-      src="https://avatars0.githubusercontent.com/u/1813468?s=460&v=4"
-    />
+    <Tag>
+      <Avatar
+        size="medium"
+        src="https://avatars0.githubusercontent.com/u/1813468?s=460&v=4"
+      />
+      Лекс Лютер
+      <Icon type="close" />
+    </Tag>
   </div>
 );
---
+++ public/scripts/components/Tag/Tag.js
@@ -0,0 +1,15 @@
+import glamorous from 'glamorous';
+
+export const Tag = glamorous.div({
+  maxWidth: '142px',
+  height: '24px',
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  paddingRight: '10px',
+  fontSize: '13px',
+  borderRadius: '16px',
+  backgroundColor: '#E9ECEF',
+  cursor: 'pointer',
+});
