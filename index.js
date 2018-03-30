1dbddcfbba15bceaf6bc5d4848ebb504e867d48d
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2018-03-30 11:04:33 +0300

    Remove redundant files, server index.html only on production mode

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const graphqlRoutes = require('./graphql/routes');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'public/build')));
-}
-
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlRoutes);
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/build/index.html'));
-});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'public/build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/build/index.html'));
+  });
+}
 
 app.listen(8080, () =>
   global.console.log('Express app listening on localhost:8080'));
--- pages/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const { index } = require('./controllers');
-
-router.get('/', index);
-
-module.exports = router;
